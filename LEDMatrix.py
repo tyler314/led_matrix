aa87aac0f14c5f482e279d1d7af27b0ca7274928
Tyler Roberts <russroberts@RussRobrtssiMac.fios-router.home>
2017-10-22 11:15:02 -0400

    Added list inheritance

--- LEDMatrix.py
+++ LEDMatrix.py
@@ -1,11 +1,5 @@
-class LEDMatrix:
+class LEDMatrix(list):
     def __init__(self, microcontroller):
         self.microcontroller = microcontroller
         rows = 16 if self.microcontroller.D is None else 32
-        self.matrix = [[0 for _ in range(32)] for _ in range(rows)]
-
-    def __getitem__(self, index):
-        return self.matrix[index[0]][index[1]]
-
-    def __setitem__(self, index, value):
-        self.matrix[index[0]][index[1]] = value
+        super().__init__([[0 for _ in range(32)] for _ in range(rows)])
