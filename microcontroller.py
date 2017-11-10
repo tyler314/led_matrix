dcd9a784310bbdace6d753b9b4dd29d1d8b64b24
Tyler Roberts <tjroberts314@gmail.com>
2017-11-09 22:49:09 -0500

    reimplemented singleton without use of metaclass

--- microcontroller.py
+++ microcontroller.py
@@ -1,10 +1,15 @@
-from singleton import Singleton
 import pyb
 import time
 
 pins = pyb.Pin.board
 
 class Microcontroller:
+    _instance = None
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
     def __init__(self):
         self.R1 = None
