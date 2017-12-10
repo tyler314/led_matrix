035e93a90b35548188e28c9850e8d1ca59dbbfea
Tyler Roberts <tjroberts314@gmail.com>
2017-12-10 17:07:48 -0500

    pep8 updates

--- microcontroller.py
+++ microcontroller.py
@@ -1,7 +1,7 @@
 import pyb
-import time
 
 pins = pyb.Pin.board
+
 
 class Microcontroller:
     _instance = None
@@ -25,25 +25,30 @@ class Microcontroller:
         self.LAT = None
         self.set_RGB_pins()
         self.set_row_select_pins()
-        self.set_control_pins()
 
     def set_RGB_pins(self, R1, R2, B1, B2, G1, G2):
         raise NotImplementedError('Need to implement set_RGB_data method.')
 
-    def set_row_select_pins(A, B, C, D):
+    def set_row_select_pins(self, A, B, C, D):
         raise NotImplementedError('Need to implement set_row_select method.')
-        
+
     def latch(self):
         self.LAT.value(0)
         pyb.udelay(500)
         self.LAT.value(1)
-            
 
 
 class Pyboard(Microcontroller):
 
-    def set_RGB_pins(self, R1=pins.Y1, R2=pins.Y2, B1=pins.Y3, B2=pins.Y4, G1=pins.Y5, G2=pins.Y6):
-        self.R1 = pyb.Pin(R1, pyb.Pin.OUT) 
+    def set_RGB_pins(
+            self,
+            R1=pins.Y1,
+            R2=pins.Y2,
+            B1=pins.Y3,
+            B2=pins.Y4,
+            G1=pins.Y5,
+            G2=pins.Y6):
+        self.R1 = pyb.Pin(R1, pyb.Pin.OUT)
         self.R2 = pyb.Pin(R2, pyb.Pin.OUT)
         self.B1 = pyb.Pin(B1, pyb.Pin.OUT)
         self.B2 = pyb.Pin(B2, pyb.Pin.OUT)
@@ -55,7 +60,7 @@ class Pyboard(Microcontroller):
         self.B = pyb.Pin(B, pyb.Pin.OUT)
         self.C = pyb.Pin(C, pyb.Pin.OUT)
         self.D = pyb.Pin(D, pyb.Pin.OUT)
-        
+
     def set_control_pins(self, LAT=pins.X11, OE=pins.X12):
         self.LAT = pyb.Pin(LAT, pyb.Pin.OUT)
         self.LAT.value(1)
