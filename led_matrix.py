b6c800fdc1161d9c449f0ca7c395774e72424363
Tyler Roberts <tjroberts314@gmail.com>
2017-12-10 19:55:19 -0500

    implemented methods for row select

--- led_matrix.py
+++ led_matrix.py
@@ -3,5 +3,37 @@ class LEDMatrix(object):
     def __init__(self, microcontroller):
         self.mc = microcontroller
 
+    def _select_row(self, row):
+        row = row % 8 if self.mc.smallBoard else row % 16
+        if row & 0x1:
+            self.mc.A.value(1)
+        else:
+            self.mc.A.value(0)
+        if row & 0x1 << 1:
+            self.mc.B.value(1)
+        else:
+            self.mc.B.value(0)
+        if row & 0x1 << 2:
+            self.mc.C.value(1)
+        else:
+            self.mc.C.value(0)
+        if row & 0x1 << 3:
+            self.mc.D.value(1)
+        else:
+            self.mc.D.value(0)
+
+    @property
+    def _turn_on(self):
+        self.mc.OE.value(0)
+
+    @property
+    def _turn_off(self):
+        self.mc.OE.value(1)
+
+    @property
+    def _toggle_on_off(self):
+        value = 0 if self.mc.OE.value() else 1
+        self.mc.OE.value(value)
+
     def set_pixel(i, j, color):
         raise NotImplementedError
--- microcontroller.py
+++ microcontroller.py
@@ -11,7 +11,7 @@ class Microcontroller:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    def __init__(self):
+    def __init__(self, smallBoard=False):
         self.R1 = None
         self.R2 = None
         self.B2 = None
@@ -23,14 +23,19 @@ class Microcontroller:
         self.D = None
         self.OE = None
         self.LAT = None
+        self.smallBoard = smallBoard
         self.set_RGB_pins()
         self.set_row_select_pins()
+        self.set_control_pins()
 
     def set_RGB_pins(self, R1, R2, B1, B2, G1, G2):
         raise NotImplementedError('Need to implement set_RGB_data method.')
 
     def set_row_select_pins(self, A, B, C, D):
         raise NotImplementedError('Need to implement set_row_select method.')
+
+    def set_control_pins(self, LAT, OE):
+        raise NotImplementedError('Need to implement set_control_pins method.')
 
     def latch(self):
         self.LAT.value(0)
